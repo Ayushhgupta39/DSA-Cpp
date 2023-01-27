74f727c4722eb0f152589f1d6ff5734c5e41eec9
Ayushhgupta39 <ayushgupta3902@gmail.com>
2023-01-27 20:15:42 +0530

    Starters 73

--- CodeChef.cpp
+++ CodeChef.cpp
@@ -1,25 +1,40 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void luckyNumber(int x) {
-    while(x != 0) {
-        int digit = x % 10;
-        if(digit == 7) {
-            cout << "Yes" << endl;
-            return;
-        }
-        x /= 10;
-    }
-    cout << "No" << endl;
-}
-
-int main(){
+int main()
+{
     int t;
     cin >> t;
-    while(t--) {
-        int x;
-        cin >> x;
-        luckyNumber(x);
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        int ones = 0;
+        for (auto i : s)
+        {
+            if (i == '1')
+            {
+                ones++;
+            }
+        }
+
+        if (s == "1" || s == "10")
+        {
+            cout << "No" << endl;
+        }
+        else
+        {
+            if (ones <= 3)
+            {
+                cout << "Yes" << endl;
+            }
+            else
+            {
+                cout << "No" << endl;
+            }
+        }
     }
-return 0;
+    return 0;
 }
