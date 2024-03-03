f2936cf70bfa6905e71778a3ae97dfb340dced22
Ayush Gupta <ayushgupta3902@gmail.com>
2024-03-03 12:55:31 +0530

    add linear search

--- array.cpp
+++ array.cpp
@@ -25,10 +25,22 @@ void arraySum(int arr[], int size) {
     cout << sum << endl;
 }
 
+void linearSearch(int arr[], int size, int elem) {
+    for (int i=0; i<size; i++) {
+        if (arr[i] == elem) {
+            cout << "Element found at: " << i << endl;
+            return;
+        }
+    }
+    cout << "Element not found" << endl;
+}
+
 int main() {
     int arr[5] = {3,5,6,7,1};
     cout << findMax(arr, 5) << endl;
     cout << findMin(arr, 5) << endl;
     arraySum(arr, 5);
+    linearSearch(arr, 5, 6);
+    linearSearch(arr, 5, 9);
     return 0;
 }
